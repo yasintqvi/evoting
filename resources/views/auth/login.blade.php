295eaab8de45a05cd84cacc820f861299f1035a4
yasin taghavi <mrtqvi@yahoo.com>
2025-01-10 09:45:48 +0330

    add login page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,75 @@
+<html lang="fa" class="js">
+
+<head>
+    <base href="../../../">
+    <meta charset="utf-8">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <!-- Fav Icon  -->
+    <link rel="shortcut icon" href="./images/favicon.png">
+    <!-- Page Title  -->
+    <title>سامانه انتخابات - ورود به حساب کاربری</title>
+    <!-- StyleSheets  -->
+    <link rel="stylesheet" href="{{ asset('assets/css/dashlite-rtl.css') }}">
+    <link id="skin-default" rel="stylesheet" href="./assets/css/theme.css">
+</head>
+
+<body class="has-rtl nk-body bg-white npc-general pg-auth no-touch nk-nio-theme" dir="rtl">
+    <div class="nk-app-root">
+        <!-- main @s -->
+        <div class="nk-main">
+            <!-- wrap @s -->
+            <div class="nk-wrap nk-wrap-nosidebar">
+                <!-- content @s -->
+                <div class="nk-content">
+                    <div class="nk-block nk-block-middle nk-auth-body wide-xs">
+                        <div class="card card-bordered">
+                            <div class="card-inner card-inner-lg">
+                                <div class="nk-block-head">
+                                    <div class="nk-block-head-content">
+                                        <h4 class="nk-block-title">ورود</h4>
+                                        <div class="nk-block-des">
+                                        </div>
+                                    </div>
+                                </div>
+                                <form action="" method="post">
+                                    <div class="form-group">
+                                        <div class="form-label-group">
+                                            <label class="form-label" for="default-01">شماره تلفن خود را وارد نمایید</label>
+                                        </div>
+                                        <div class="form-control-wrap">
+                                            <input type="text" style="direction: ltr;" class="form-control form-control-lg" id="default-01" placeholder="9111235566">
+                                        </div>
+                                    </div>
+                                    <!-- <div class="form-group">
+                                        <div class="form-label-group">
+                                            <label class="form-label" for="password">رمز عبور</label>
+                                            <a class="link link-primary link-sm" href="html/pages/auths/auth-reset-v2.html">رمز عبور را فراموش کردید؟</a>
+                                        </div>
+                                        <div class="form-control-wrap">
+                                            <a href="#" class="form-icon form-icon-right passcode-switch lg" data-target="password">
+                                                <em class="passcode-icon icon-show icon ni ni-eye"></em>
+                                                <em class="passcode-icon icon-hide icon ni ni-eye-off"></em>
+                                            </a>
+                                            <input type="password" class="form-control form-control-lg" id="password" placeholder="رمز عبور خود را وارد کنید">
+                                        </div>
+                                    </div> -->
+                                    <div class="form-group">
+                                        <button class="btn btn-lg btn-primary btn-block">ورود</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- wrap @e -->
+            </div>
+            <!-- content @e -->
+        </div>
+        <!-- main @e -->
+    </div>
+    <!-- app-root @e -->
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\OtpController;
 use Illuminate\Support\Facades\Route;
 
 
