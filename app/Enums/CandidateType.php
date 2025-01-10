c5d7db485448900b88a07a7c615a78d76c87c7de
yasin taghavi <mrtqvi@yahoo.com>
2025-01-10 08:51:45 +0330

    fix: candidate table fields

---
+++ app/Enums/CandidateType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum CandidateType: int
+{
+    case DIRECTOR = 1;
+    case INSPECTOR = 2;
+}
--- database/migrations/2025_01_09_063211_create_candidates_table.php
+++ database/migrations/2025_01_09_063211_create_candidates_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\CandidateType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +16,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users');
             $table->foreignId("election_id")->constrained('elections');
+            $table->tinyInteger('candidate_type')->default(CandidateType::DIRECTOR);
             $table->timestamps();
         });
     }
