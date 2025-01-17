3538599fb2c66881147ba36f457cc7bab363fc0d
AlirezaAk <alirezaak@AlirezaAks-MacBook-Air.local>
2025-01-17 14:30:53 +0330

    add table session

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\GroupController;
 use Illuminate\Support\Facades\Route;
 
 
-
 Route::middleware('guest')->group(function () {
 
     Route::get('/login', [AuthController::class, 'loginForm'])->name('login.form');
