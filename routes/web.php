8b7c75ca390b4ad52225c6e0ae4d371a30d156a2
AlirezaAk <alirezaak@AlirezaAks-MacBook-Air.local>
2025-02-03 22:34:59 +0330

    edit name controller adn route

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,13 @@ use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\ElectionController;
 use App\Http\Controllers\ElectionUserController;
 use App\Http\Controllers\GroupController;
+use App\Http\Controllers\GroupUserController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserExcelController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Auth::loginUsingId(1);
 
 Route::middleware('guest')->group(function () {
 
@@ -61,7 +62,7 @@ Route::middleware('auth')->group(function () {
 
             Route::get('/candidates', fn() => view('app.election.candidate.index'));
           
-            Route::resource('/election-users', ElectionUserController::class);
+            Route::resource('/election-users', GroupUserController::class);
         });
     });
 });
