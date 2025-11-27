71650ce16a2c61ddc5a80146dfc8def9a4623475
alirezaak <akcode001@gmail.com>
2025-11-27 09:58:14 +0330

    bug fix ui

--- resources/views/app/layouts/partials/group-sidebar.blade.php
+++ resources/views/app/layouts/partials/group-sidebar.blade.php
@@ -43,6 +43,9 @@
                     <span class="menu-text"> داشبورد </span>
                 </a>
             </li>
+
+
+
             @can(\App\Enums\Permission::VIEW_GROUP_USERS->value)
                 <li class="side-nav-item">
                     <a href="{{ route('group.users.index', $group) }}" class="side-nav-link">
@@ -58,64 +61,78 @@
                         <span class="menu-text"> اعضا </span>
                     </a>
                 </li>
-                <br>
-            @can(\App\Enums\Permission::VIEW_GROUP_EVENT->value)
-                <li class="side-nav-title">لیست آخرین رویداد ها</li>
-                @forelse($group->events()->latest()->take(5)->get() as $event)
-                    <li class="side-nav-item active">
-                        <a data-bs-toggle="collapse" href="#event-{{ $event->id }}" aria-expanded="false"
-                            aria-controls="sidebarHospital" class="side-nav-link collapsed">
+                @can(\App\Enums\Permission::VIEW_GROUP_EVENT->value)
+                    <li class="side-nav-item">
+                        <a href="{{ route('events.index', $group) }}" class="side-nav-link">
                             <span class="menu-icon">
-                                @if ($event->logo)
-                                    <img src="{{ asset($event->logo) }}"
-                                        class="rounded-circle me-lg-2 d-flex object-fit-cover" width="20" height="20"
-                                        alt="{{ $group->title }}">
-                                @else
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="1.5" stroke="currentColor" class="size-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
-                                    </svg>
-                                @endif
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-calendar3-event-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3h16zm-3-9a1 1 0 1 0 0 2 1 1 0 0 0 0-2m1-5a2 2 0 0 1 2 2H0a2 2 0 0 1 2-2z" />
+                                </svg>  
                             </span>
-                            <span class="menu-text text-wrap small" style="line-height: 1.2rem;">
-                                {{ $event->title }}
-                            </span>
-                            <span class="menu-arrow"></span>
+                            <span class="menu-text"> نمایش همه رویدادها </span>
                         </a>
-
-                        <div class="collapse" id="event-{{ $event->id }}">
-                            <ul class="sub-menu">
-                                <li class="side-nav-item">
-                                    <a href="{{ route('attendances.create', [$group, $event]) }}" class="side-nav-link">
-                                        <span class="menu-text">حضور و غیاب</span>
-                                    </a>
-                                </li>
-                                <li class="side-nav-item">
-                                    <a href="{{ route('elections.index', [$group, $event]) }}" class="side-nav-link">
-                                        <span class="menu-text">انتخابات</span>
-                                    </a>
-                                </li>
-                                <li class="side-nav-item">
-                                    <a href="{{ route('surveys.index', [$group, $event]) }}" class="side-nav-link">
-                                        <span class="menu-text">نظرسنجی‌ها</span>
-                                    </a>
-                                </li>
-                                <li class="side-nav-item">
-                                    <a href="{{ route('group.event.show', [$group, $event]) }}" class="side-nav-link">
-                                        <span class="menu-text">نمایش جزئیات</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
                     </li>
-                @empty
-                    <div class="d-flex justify-content-center align-items-center">
-                        <div class="d-flex flex-column">
-                            <div class="mt-2 side-nav-title"> هنوز هیچ رویدادی اضافه نشده است.</div>
+                @endcan
+                <br>
+                @can(\App\Enums\Permission::VIEW_GROUP_EVENT->value)
+                    <li class="side-nav-title">لیست آخرین رویداد ها</li>
+                    @forelse($group->events()->latest()->take(5)->get() as $event)
+                        <li class="side-nav-item active">
+                            <a data-bs-toggle="collapse" href="#event-{{ $event->id }}" aria-expanded="false"
+                                aria-controls="sidebarHospital" class="side-nav-link collapsed">
+                                <span class="menu-icon">
+                                    @if ($event->logo)
+                                        <img src="{{ asset($event->logo) }}"
+                                            class="rounded-circle me-lg-2 d-flex object-fit-cover" width="20" height="20"
+                                            alt="{{ $group->title }}">
+                                    @else
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="size-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                                        </svg>
+                                    @endif
+                                </span>
+                                <span class="menu-text text-wrap small" style="line-height: 1.2rem;">
+                                    {{ $event->name }}
+                                </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+
+                            <div class="collapse" id="event-{{ $event->id }}">
+                                <ul class="sub-menu">
+                                    <li class="side-nav-item">
+                                        <a href="{{ route('attendances.create', [$group, $event]) }}" class="side-nav-link">
+                                            <span class="menu-text">حضور و غیاب</span>
+                                        </a>
+                                    </li>
+                                    <li class="side-nav-item">
+                                        <a href="{{ route('elections.index', [$group, $event]) }}" class="side-nav-link">
+                                            <span class="menu-text">انتخابات</span>
+                                        </a>
+                                    </li>
+                                    <li class="side-nav-item">
+                                        <a href="{{ route('surveys.index', [$group, $event]) }}" class="side-nav-link">
+                                            <span class="menu-text">نظرسنجی‌ها</span>
+                                        </a>
+                                    </li>
+                                    <li class="side-nav-item">
+                                        <a href="{{ route('group.event.show', [$group, $event]) }}" class="side-nav-link">
+                                            <span class="menu-text">نمایش جزئیات</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                    @empty
+                        <div class="d-flex justify-content-center align-items-center">
+                            <div class="d-flex flex-column">
+                                <div class="mt-2 side-nav-title"> هنوز هیچ رویدادی اضافه نشده است.</div>
+                            </div>
                         </div>
-                    </div>
-                @endforelse
+                    @endforelse
 
                 @endcan
                 @can(\App\Enums\Permission::CREATE_GROUP_EVENT->value)
@@ -124,14 +141,10 @@
                             جدید</a>
                     </div>
                 @endcan
-                @can(\App\Enums\Permission::VIEW_GROUP_EVENT->value)
-                    <div class="d-flex flex-column mt-3">
-                        <a href="{{ route('events.index', $group) }}" class="btn btn-secondary btn-sm mx-auto">نمایش همه</a>
-                    </div>
-                @endcan
 
 
             @endcan
+
         </ul>
 
 
